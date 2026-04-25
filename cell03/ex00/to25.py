5d51638db84ebeb5d736f33c274a231191a3638b
pleumtana <pleumtana@gmail.com>
2026-04-25 06:28:51 +0000

    pp

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+number=int(input("Number:"))
+if number>25:
+    print("error")
+else :
+    while number < 25 :
+        print("Inside the loop, my variable is " ,number)
+        number=number+1
+        
+    
