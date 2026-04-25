ee3092923d63ecdbad65d00fb1322067870baff6
pleumtana <pleumtana@gmail.com>
2026-04-25 03:46:55 +0000

    pp

---
+++ cell02/ex01/isnge.py
@@ -0,0 +1,10 @@
+x=int(input("Number:"))
+
+if x<0:
+    print("This number is negative.")
+
+if x>0:
+    print("This number is positive.")
+
+if x==0:
+    print("This number is both positive and negative.")
