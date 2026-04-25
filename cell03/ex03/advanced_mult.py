044f2fdc823c9fba8dc2ed5b9b75f4ad171eef30
pleumtana <pleumtana@gmail.com>
2026-04-25 09:15:39 +0000

    advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 import sys
 y=0
+a=0
 if len(sys.argv) > 1:
     arg = sys.argv[1]
 
     if arg.isdigit():
-       pass
+       sys.exit()    
     else:
         print("none")
-        
-for y in range(11):
-    print("table be",y, end=" :")
-    x=0
-    while x <= 10:        
-        print(x*y , end=' ')
-        x=x+1
-    print()
+        a=1
+     
+if a==0:
+    for y in range(11):
+        print("table be",y, end=" :")
+        x=0
+        while x <= 10:        
+            print(x*y , end=' ')
+            x=x+1
+        print()
+else:
+    pass
