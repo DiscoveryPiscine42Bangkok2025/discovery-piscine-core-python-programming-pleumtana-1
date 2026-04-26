a76828e859e8e7246e33568c7c0bdbcbda0993f1
pleumtana <pleumtana@gmail.com>
2026-04-26 03:28:38 +0000

    called calculator.py

---
+++ cell04/ex02/called calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+x=int(input("Number1 :"))
+y=int(input("Number2 :"))
+print("Thank you!")
+print(f"{x}+{y} =",x+y)
+print(f"{x}-{y} =",x-y)
+print(f"{x}*{y} =",x*y)
+print(f"{x}/{y} =",x//y)
